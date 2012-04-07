f51c02da97dbff8971d67e7053b4e59b2690aff9
Filutki <filutkie@gmail.com>
2012-04-07 23:00:16 +0300

    Night update

--- src/recycle/me/RecycleMeActivity.java
+++ src/recycle/me/RecycleMeActivity.java
@@ -37,7 +37,11 @@ public class RecycleMeActivity extends MapActivity implements OnClickListener {
 		GeoPoint point = new GeoPoint(46484921, 30734484);
 		OverlayItem overlayitem = new OverlayItem(point, "Hola, Mundo!", "I'm in Odessa!");
 		
+		GeoPoint point2 = new GeoPoint(35410000, 139460000);
+		OverlayItem overlayitem2 = new OverlayItem(point2, "Sekai, konichiwa!", "I'm in Japan!");
+		
 		bio.addOverlay(overlayitem);
+		bio.addOverlay(overlayitem2);
 		mapOverlays.add(bio);
 		//bio.onTap(0);
 		
@@ -45,7 +49,7 @@ public class RecycleMeActivity extends MapActivity implements OnClickListener {
 		
 		// Координаты пишутся без точки после первых двух чисел
 		// center to city park
-		mMapController.animateTo((new GeoPoint(46484921, 30734484)));
+		mMapController.animateTo(point);
 		 
 		//1 is world view, 21 is the tightest zoom possible
 		mMapController.setZoom(15);
@@ -58,15 +62,20 @@ public class RecycleMeActivity extends MapActivity implements OnClickListener {
 	}
 
 	public void onClick(View v) {
+		Drawable img; 
 		// to sat
 		if (flag) {
 			btnMap.setText("Map");
+			img = getResources().getDrawable(R.drawable.mapicon);
+			btnMap.setCompoundDrawablesWithIntrinsicBounds(img, null, null, null);
 			mapView.setSatellite(true);
 			Toast.makeText(this, "Switched to satellite view",
 					Toast.LENGTH_SHORT).show();
 			// to map
 		} else if (!flag) {
 			btnMap.setText("Satellite");
+			img = getResources().getDrawable(R.drawable.saticon);
+			btnMap.setCompoundDrawablesWithIntrinsicBounds(img, null, null, null);
 			mapView.setSatellite(false);
 			Toast.makeText(this, "Switched to map view", Toast.LENGTH_SHORT)
 					.show();
